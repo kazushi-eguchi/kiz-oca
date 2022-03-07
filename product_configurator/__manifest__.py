16333e53f1f8191831a320bfb3a67bd1548ed8bb
Kazushi Eguchi <sandwhale1010@gmail.com>
2022-03-07 10:40:42 +0900

    更新

--- product_configurator/__manifest__.py
+++ product_configurator/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Product Configurator",
-    "version": "14.0.1.2.2",
+    "version": "14.0.1.2.3",
     "category": "Generic Modules/Base",
     "summary": "Base for product configuration interface modules",
     "author": "Pledra, Odoo Community Association (OCA)",
@@ -33,7 +33,7 @@
     "post_init_hook": "post_init_hook",
     "qweb": ["static/xml/create_button.xml"],
     "development_status": "Beta",
-    "maintainers": ["pcatinean"],
+    "maintainers": ["PCatinean"],
     "installable": True,
     "application": True,
     "auto_install": False,
